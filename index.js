f24d40fedaadf39cdfdb8b75154ddd1aa02e066d
Shashank Bansal <bansalshashank01@gmail.com>
2024-03-17 00:33:26 +0530

    changed the service worker registration path from "/service-worker.js" to "./service-worker.js"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   // register service worker
   navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((res) => console.log("service worker registered"))
+    .register("./service-worker.js")
+    .then((res) => console.log("service worker registered", res))
     .catch((err) => console.log("service worker not registered", err));
 }
